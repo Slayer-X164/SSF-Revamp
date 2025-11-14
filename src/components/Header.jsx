5af64a519f3fb815d36d74ef3e7327be117b1a1f
Siddhesh <siddheshghag150@gmail.com>
2025-11-14 18:12:57 +0530

    init: repo initialized

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { IoIosArrowDown } from 'react-icons/io'
+
+const Header = () => {
+  return (
+    <div className='flex w-7xl  items-center justify-between h-30'>
+        <a href="#home">
+            <img src="logo.svg" alt="logo" />
+        </a>
+        <ul className='flex items-center gap-10 text-lg uppercase font-semibold'>
+            <li className='cursor-pointer'><a href="#home">Home</a></li>
+            <li className='cursor-pointer'><a href="#about-us">About Us</a></li>
+            <li className='cursor-pointer'><a href="#campaign">Campaign</a></li>
+            <li className='cursor-pointer'><a href="#join-us">Join Us</a></li>
+            <li className='flex items-center gap-1 cursor-pointer'>More<IoIosArrowDown /></li>
+        </ul>
+        <button  className='px-4 py-2 bg-[#005EFF] text-neutral-50 hover:bg-black hover:text-neutral-50 font-semibold text-lg relative group transition-all duration-200 ease-in cursor-pointer'
+        ><a href="#">Donate Now <span class="absolute group-hover:translate-x-0 group-hover:translate-y-0 inset-0 bg-black translate-x-1.5 translate-y-1.5 -z-1 transition-all duration-200 ease-in"></span></a></button>
+    </div>
+  )
+}
+
+export default Header
