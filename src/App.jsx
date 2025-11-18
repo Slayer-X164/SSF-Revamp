debcbfc4035386c3e925c60886a50a2644c2e4c7
Naim Shaikh <nn9412466@gmail.com>
2025-11-18 17:15:39 +0530

    added-KeyFocus-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Getinvolved from './components/GetInvolved'
+import KeyFocus from './pages/KeyFocus'
 
 const App = () => {
   return (
     <div className='min-h-screen w-full flex justify-start flex-col items-center font-inria px-2 overflow-x-hidden'>
+      {/* Header Page */}
       <Header/>
+      {/* Home Page */}
       <Home/>
+      {/* Get Involved Page */}
       <Getinvolved/>
+      {/* Key focus Page */}
+      <KeyFocus />
     </div>
   )
 }
